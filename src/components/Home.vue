341be32d8979eff3a0599b17756b62194a7e1bae
n.see <gooarter@gmail.com>
2018-10-25 13:16:57 +0800

    edit home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,94 +1,16 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg | capitalize }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+  <div class="home">
+      <h2>{{ msg }}</h2>
+      <router-link to="/admin" >admin</router-link>
   </div>
 </template>
 
 <script>
 export default {
-    name: 'HelloWorld',
+    name: 'home',
     data() {
         return {
-            msg: 'welcome to Your Vue.js App'
+            msg: 'welcome to Your Vued App'
         };
     }
 };
@@ -96,6 +18,13 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+    .home{
+        text-align: center;
+    }
+    h2{
+
+        line-height: 300px;
+    }
 h1, h2 {
   font-weight: normal;
 }
